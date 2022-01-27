fc7345de057e647614a056e2eaecd3950c272985
eliorpacarada1 <eliorpacarada1@gmail.com>
2022-01-27 14:21:07 +0100

    Unit tests for delete method

--- UnitTests/BetUnitTests/BetServiceUnitTests.cs
+++ UnitTests/BetUnitTests/BetServiceUnitTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using BettingApp.Dtos.Requests;
 using BettingApp.Dtos.Responses;
 using BettingApp.Models;
@@ -11,6 +6,9 @@ using BettingApp.Repositories;
 using BettingApp.Services;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnitTests.BetUnitTests.Helper;
 using Xunit;
 
@@ -53,7 +51,7 @@ namespace UnitTests.BetUnitTests
             _betRepositoryMock.Setup(x => x.CreateBet(It.IsAny<Bet>())).ReturnsAsync(bet);
             _mapperMock.Setup(x => x.Map<Bet>(It.IsAny<BetCreateRequest>())).Returns(bet);
             _mapperMock.Setup(x => x.Map<BetCreateResponse>(It.IsAny<Bet>())).Returns(betCreateResponse);
-            
+
             //Act - Qetu thirret metoda e service. Kthen resultin qe nbaz qysh i kena mock na senet na nbaz tlogjikes ka me kthy diqka
             var result = await _sut.CreateBet(betCreateRequest);
 
@@ -66,11 +64,11 @@ namespace UnitTests.BetUnitTests
         public async Task CreateBet_InvalidInformation_BetNotCreated()
         {
             //Arrange
-           
+
             //Act
 
             //Assert
-            
+
         }
 
         [Fact]
@@ -131,8 +129,30 @@ namespace UnitTests.BetUnitTests
         [Fact]
         public async Task DeleteBet_ValidData_BetDeleted()
         {
+
             //Arrange
+            _betRepositoryMock.Setup(x => x.DeleteBet(It.IsAny<Bet>())).ReturnsAsync(true);
+            _betRepositoryMock.Setup(x => x.GetBetById(It.IsAny<Guid>())).ReturnsAsync(bet);
+
+            //Act
+            var result = await _sut.DeleteBet(bet.Id);
+            
+            //Assert
+            Assert.True(result);
+        }
+        [Fact]
+        public async Task DeleteBet_InValidData_BetNotDeleted()
+        {
+            //Arrange
+            _betRepositoryMock.Setup(x => x.GetBetById(It.IsAny<Guid>())).ReturnsAsync((Bet)null);
+            
+            //Act
+            var result = await _sut.DeleteBet(bet.Id);
+            
+            //Assert
+            Assert.False(result);
 
         }
+
     }
 }
